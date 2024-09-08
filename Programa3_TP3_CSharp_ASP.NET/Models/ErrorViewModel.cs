a739337c080e7de738e18fb6e18f94665041666d
FeHunter <hunterfe1111@gmail.com>
2024-09-08 10:40:14 -0300

    Adicionar arquivos de projeto.

---
+++ Programa3_TP3_CSharp_ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Programa3_TP3_CSharp_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
